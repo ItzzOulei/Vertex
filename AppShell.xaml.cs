59ec4c7ea9337219ead7b4b679653e14c0d00c0c
Oulei <olivier.deszynski@ict-campus.net>
2025-12-04 14:02:46 +0100

    SetUp Project Folder

---
+++ AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace Vertex;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vertex")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "Vertex.Pages")]
